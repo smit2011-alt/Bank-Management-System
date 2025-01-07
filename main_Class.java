ae43ea0a83e11dafdcbc49002a7d48520b1cb3a5
smit2011-alt <mallyabhai77@gmail.com>
2025-01-07 22:41:02 +0530

    Update main_Class.java

--- main_Class.java
+++ main_Class.java
@@ -102,8 +102,10 @@ public class main_Class extends JFrame implements ActionListener {
         }else if(e.getSource()==b5){
             new Pin(pin);
             setVisible(false);
-        }
+        }else if(e.getSource()==b4){
+            new mini(pin);
     }
+}
 
     public static void main(String[] args){
         new main_Class("");
